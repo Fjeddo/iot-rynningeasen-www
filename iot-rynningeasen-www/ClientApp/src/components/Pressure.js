07b69ac6d254d6fcbc7245558ce5778aaa91e023
Fredrik Johnsson <fredrik.johnsson7@gmail.com>
2019-02-04 22:38:43 +0100

    Update Pressure.js

--- iot-rynningeasen-www/ClientApp/src/components/Pressure.js
+++ iot-rynningeasen-www/ClientApp/src/components/Pressure.js
@@ -6,7 +6,7 @@ class Pressure extends React.Component {
         return (
             <div>
                 <h2>Pressure charts</h2>
-                <IFrame src={'https://api.thingspeak.com/channels/693482/charts/1?title=Senaste+timmen&width=auto&height=400&results=60&dynamic=true'}/>
+                <IFrame src={'https://api.thingspeak.com/channels/693482/charts/2?title=Senaste+timmen&width=auto&height=400&results=60&dynamic=true'}/>
                 <IFrame src={'https://api.thingspeak.com/channels/661241/charts/1?title=Senaste+dygnet&width=auto&height=400&results=1440&dynamic=true&average=30'}/>
                 <IFrame src={'https://api.thingspeak.com/channels/661241/charts/1?title=Senaste+veckan&width=auto&height=400&results=10080&dynamic=true&average=240'}/>
             </div>
