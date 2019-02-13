dc13381c1fb511956d9473e7ce271c7ad3a7e748
Fredrik Johnsson <fredrik.johnsson7@gmail.com>
2019-02-13 08:03:12 +0100

    Update Temperature.js

--- iot-rynningeasen-www/ClientApp/src/components/Temperature.js
+++ iot-rynningeasen-www/ClientApp/src/components/Temperature.js
@@ -9,7 +9,7 @@ class Temperature extends React.Component {
                 <h2>Temperature charts</h2>
                 <IFrame src={'https://api.thingspeak.com/channels/693480/charts/1?title=Senaste+timmen&width=auto&height=400&results=60&dynamic=true'}/>
                 <IFrame src={'https://api.thingspeak.com/channels/693480/charts/1?title=Senaste+dygnet&width=auto&height=400&days=1&average=20'}/>
-                <IFrame src={'https://api.thingspeak.com/channels/693480/charts/1?title=Senaste+veckan&width=auto&height=400&days=7&average=240'}/>
+                <IFrame src={'https://api.thingspeak.com/channels/693480/charts/1?title=Senaste+veckan&width=auto&height=400&days=7&average=60'}/>
             </div>
         );
     }
