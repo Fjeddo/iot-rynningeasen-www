377ce84da8348289743b8606041f6f3324036432
Fredrik Johnsson <fredrik.johnsson7@gmail.com>
2019-05-28 20:56:12 +0200

    Update Home.js

--- iot-rynningeasen-www/ClientApp/src/components/Home.js
+++ iot-rynningeasen-www/ClientApp/src/components/Home.js
@@ -11,7 +11,7 @@ export class Home extends Component {
       <div>
         <h1>Current measurements @ Rynningeåsen</h1>
         <CurrentTemperature />
-        <IFrame src={'https://api.thingspeak.com/channels/693480/charts/1?title=Senaste+timmen&width=auto&height=400&days=1&dynamic=true'}/>
+        <IFrame src={'https://api.thingspeak.com/channels/693480/charts/1?title=Senaste+dygnet&width=auto&height=400&days=1&dynamic=true'}/>
         //<CurrentPressure />
         //<IFrame src={'https://api.thingspeak.com/channels/693482/charts/2?title=Senaste+timmen&width=auto&height=400&results=60&dynamic=true'}/>
       
