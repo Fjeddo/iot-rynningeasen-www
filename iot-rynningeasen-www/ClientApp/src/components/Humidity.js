92f5ace4f713a02ff26da858b27495a3c30a4fdb
Fredrik Johnsson <fredrik.johnsson7@gmail.com>
2019-06-23 22:43:14 +0200

    Update Humidity.js

--- iot-rynningeasen-www/ClientApp/src/components/Humidity.js
+++ iot-rynningeasen-www/ClientApp/src/components/Humidity.js
@@ -7,7 +7,6 @@ class Humidity extends React.Component {
         return (
             <div>
                 <CurrentHumidity />
-                <h2>Humidity charts</h2>
                 <IFrame src={'https://api.thingspeak.com/channels/796905/charts/3?title=Senaste+timmen&width=auto&height=400&results=60&dynamic=true'}/>
                 <IFrame src={'https://api.thingspeak.com/channels/796905/charts/3?title=Senaste+dygnet&width=auto&height=400&days=1&average=20&dynamic=true'}/>
                 <IFrame src={'https://api.thingspeak.com/channels/796905/charts/3?title=Senaste+veckan&width=auto&height=400&days=7&average=60&dynamic=true'}/>
