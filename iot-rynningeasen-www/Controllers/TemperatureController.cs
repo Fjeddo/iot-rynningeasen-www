62fb7b4cf2d7aefaaecd2642c822dcbe964c4e38
Fredrik Johnsson <fredrik.johnsson7@gmail.com>
2019-09-24 09:35:47 +0200

    Update TemperatureController.cs

--- iot-rynningeasen-www/Controllers/TemperatureController.cs
+++ iot-rynningeasen-www/Controllers/TemperatureController.cs
@@ -5,9 +5,16 @@ namespace IoTRynningeasenWWW.Controllers
     [Route("api/[controller]")]
     public class TemperatureController : Controller
     {
+        [Route("")]
         public ActionResult<string> Get()
         {
             return Ok($"{MeasurementsController.Measurements.Temperature.Current.What:F1}");
+        }
+        
+        [Route("details")]
+        public ActionResult<string> Details()
+        {
+            return Ok($"{MeasurementsController.Measurements.Temperature.Current.What:F1} @ {MeasurementsController.Measurements.Temperature.Current.When}");
         }
     }
 }
